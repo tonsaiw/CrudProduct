8169b84252f3c24fbf48f91a6cb1a7e8206f26e0
tonsaiw <tonsai10538@gmail.com>
2024-10-11 16:19:59 +0700

    create CRUD products with GraphQL (code first)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: true,
+  }),ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
